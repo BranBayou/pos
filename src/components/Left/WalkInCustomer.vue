84824b90a8dcb1eb6b42406fe29e492523211cf6
Berhanu Baye <berhan.baye@gmail.com>
2024-08-29 19:12:58 +0300

    Add walk in customer component

---
+++ src/components/Left/WalkInCustomer.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <button><i></i><i class="pi pi-user"></i> Walk In Customer</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
