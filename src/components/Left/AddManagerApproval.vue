21b1e7abf9083df57960f3c2faeee0cc8a88701c
Berhanu Baye <berhan.baye@gmail.com>
2024-08-29 19:19:08 +0300

    Add manager approvals component

---
+++ src/components/Left/AddManagerApproval.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <button><i class="pi pi-user"></i>Add Manager Approvals</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
