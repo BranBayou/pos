fa79c9bf44ca32296f388dff0fd4cf729492abe3
Berhanu Baye <berhan.baye@gmail.com>
2024-09-02 23:58:10 +0300

    Add item functionality work

--- src/components/Right/AddedProduct.vue
+++ src/components/Right/AddedProduct.vue
@@ -1,15 +1,31 @@
 <script setup>
+import { useOrderStore } from '@/stores/OrderStore';
 
+const orderStore = useOrderStore();
 </script>
 
 <template>
-    <div class="flex items-center justify-between rounded-2xl shadow-xl m-3 py-5 px-3">
-        <img src="" alt="product-img">
-        <h2>Men's Traditional Work Pants </h2>
-        <p>$169</p>
+  <div>
+    <!-- Check if there are any order items -->
+    <div v-if="orderStore.getOrderItems.length > 0">
+      <!-- Loop through each order item -->
+      <div 
+        v-for="(item, index) in orderStore.getOrderItems" 
+        :key="index" 
+        class="flex items-center justify-between rounded-2xl shadow-xl m-3 py-5 px-3"
+      >
+        <img :src="item.imageUrl" alt="product-img">
+        <h2>{{ item.Name }}</h2>
+        <p>${{ item.Price }}</p>
+      </div>
     </div>
+    <!-- Display a message if there are no orders -->
+    <div v-else>
+      <h2>No Orders!</h2>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+/* Add any scoped styles here */
 </style>
