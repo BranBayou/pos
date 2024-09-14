5a2a073f68c98e6bfff9ad0bd265cd0f8455455f
Berhanu Baye <berhan.baye@gmail.com>
2024-09-14 20:29:26 +0300

    Add selected customer working state

--- src/components/Left/WalkInCustomerButton.vue
+++ src/components/Left/WalkInCustomerButton.vue
@@ -22,8 +22,11 @@ const handleWalkInCustomer = () => {
         <button
          @click="handleWalkInCustomer" 
          class="border shadow-lg text-center p-5 rounded-2xl flex gap-5 my-3 w-full">
-            <span class="flex items-center gap-10 text-center cursor-pointer">
-                <i class="pi pi-user text-purple-500 bg-purple-100 p-4 rounded-full" style="font-size: 1.875rem;"></i>
+            <span class="flex items-center gap-5 text-center cursor-pointer">
+                <div class="flex items-center">
+                    <img class="w-14 h-14" src="/walking.png" alt="Walking Image">
+                    <i class="pi pi-user text-purple-500 bg-purple-100 p-4 rounded-full" style="font-size: 1.875rem;"></i>
+                </div>
                 <!-- Check if a customer is selected and show name and phone or fallback to 'Select Customer' -->
                 <p 
                  v-if="selectedCustomer"
