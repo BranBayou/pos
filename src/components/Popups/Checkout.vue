0d7560ceb715fe8997e500f00c4a9f7fb2d5484c
Berhanu Baye <berhan.baye@gmail.com>
2024-09-30 09:24:19 +0300

    update ui

--- src/components/Popups/Checkout.vue
+++ src/components/Popups/Checkout.vue
@@ -212,6 +212,7 @@ async function handleCheckout() {
                           v-if="method.amount > 0" 
                           @click="clearPaymentAmount(method)"
                           class="absolute right-2 top-2 text-gray-500 hover:text-gray-700 text-lg"
+                          style="font-size: 14px;"
                         >
                           <i class="pi pi-times"></i>
                         </button>
